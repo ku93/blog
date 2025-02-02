6ad6fd97dec6a6e54a9c4d6d398e39e6a1d36d60
ku93 <tkachev1993adg@yandex.ru>
2025-02-02 13:15:15 +0300

    v.0.0

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "blog",
+    'django.contrib.humanize',
+    'blog',
 ]
 
 MIDDLEWARE = [
